c473a1de3aa5fcf531bc92e8ae02a1c407adab1f
Chad Pearson <G00254194@stu.raritanval.edu>
2018-09-12 17:24:37 -0400

    Finished Apple script
    Deletes apples that fall below a certain height

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Apple : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static float bottomY = -20f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
