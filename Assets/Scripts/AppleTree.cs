0515f803feb612ce90ad070d03f8000b64eaa2c4
Chad Pearson <G00254194@stu.raritanval.edu>
2018-09-12 15:55:25 -0400

    Set up appletree actions

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript2 : MonoBehaviour {
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
 
 	// Use this for initialization
 	void Start () {
